5b5ce14cce3ad5083148e881c16eee12ba284bbb
newdevuniverse <112175135+newdevuniverse@users.noreply.github.com>
2022-11-21 13:30:57 +0530

    Add files via upload

---
+++ Nodejs/app.js
@@ -0,0 +1,6 @@
+//import superheroes from 'superheroes';   or use require to Import
+const superheroes=require('superheroes');
+
+//console.log (3+3);
+//const superheroes = require('superheroes');
+console.log(superheroes.random());
